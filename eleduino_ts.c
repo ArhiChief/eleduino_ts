7993b0fdfe1857226e4f8aea171f5b5db276dfe5
ArhiChief <arhichief@gmail.com>
2015-11-07 00:33:09 +0300

    Fix some errors

--- eleduino_ts.c
+++ eleduino_ts.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * Should you need to contact me, the author, you can do so either by
- * e-mail - mail your message to <varhichief@gmail.com>.
+ * e-mail - mail your message to <arhichief@gmail.com>.
  */
 
 #include <linux/kernel.h>
